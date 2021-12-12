6dbd19e4c13757ab15295c27b34e0aa435225f48
Juanse Villegas <juanvjaramillo@gmail.com>
2021-12-12 10:09:24 -0500

    Se agrega la variable RECORDS_BY_PAGE al archivo del config. PD Este en teoría se debería agregar al .gitignore

--- src/app/config/general-data.ts
+++ src/app/config/general-data.ts
@@ -1,8 +1,9 @@
 export namespace GeneralData{
     export const ADMIN_USERS_URL = "http://localhost:4200";
+    export const BUSSINESS_URL = "http://localhost:3000";
     export const EMAIL_MIN_LENGTH = 10;
     export const PASSWORD_MIN_LENGTH = 6;
     export const PASSWORD_MAX_LENGTH = 16;
+    export const RECORDS_BY_PAGE = 10;
     export const INVALID_FROM_MESSAGE = "Campos insatisfechos. Verifique la información diligenciada.";
-    export const BUSSINESS_URL = "http://localhost:4200";
 }
