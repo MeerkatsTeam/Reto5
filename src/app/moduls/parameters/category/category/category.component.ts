fa3ed3159815ab8c2997093bd2e38ba173969338
Juanse Villegas <juanvjaramillo@gmail.com>
2021-12-12 09:47:18 -0500

    Archivo de category component con funciones creadas

--- src/app/moduls/parameters/category/category/category.component.ts
+++ src/app/moduls/parameters/category/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralData } from 'src/app/config/general-data';
+import { CategoryModel } from 'src/app/models/category.model';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  pageSize: number = GeneralData.RECORDS_BY_PAGE;
+  p: number = 1;
+  total: number = 0;
+  recordList: CategoryModel[] = [];
 
-  constructor() { }
+  constructor(private service: CategoryService) { }
 
   ngOnInit(): void {
+    this.GetRecordList();
   }
 
+  GetRecordList(){
+    this.service.GetRecordList().subscribe({
+      next: (data: CategoryModel[]) =>{
+        this.recordList = data;
+        this.total = this.recordList.length;
+      }
+    })
+  }
 }
